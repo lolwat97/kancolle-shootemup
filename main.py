3ed31f47f635fce325c860aab1b1b8113bc78586
Sergey Sofeychuk <lw@fmap.me>
2016-05-24 06:54:11 +0300

    Added player-projectile collision, and different waves of enemies with autospawn

--- main.py
+++ main.py
@@ -4,22 +4,30 @@ pygame.init()
 
 windowSize = windowWidth, windowHeight = 640, 640
 screen = pygame.display.set_mode(windowSize)
-pygame.display.set_caption('fuck')
+pygame.display.set_caption('fuck') #TODO: change
 
 clock = pygame.time.Clock()
 
 class Player:
-    def __init__(self, posx, posy):
+    def __init__(self, posx, posy, lives):
         self.posx = posx
         self.posy = posy
+        self.lives = lives
     graphics = []
     graphics.append(pygame.transform.scale(pygame.image.load('tenryuu0.png'), (80, 80)))
     graphics.append(pygame.transform.scale(pygame.image.load('tenryuu1.png'), (80, 80)))
     animationCycle = 0
-    def draw(self, screen):
+    def draw(self, screen): #this thing right here slows down the animation
         screen.blit(self.graphics[self.animationCycle // 8], (self.posx, self.posy))
         self.animationCycle += 1
         self.animationCycle = self.animationCycle % 16
+    def checkProjectileCollisions(self, projectiles):
+        for projectile in projectiles.projectiles: #check collisions with projectiles, projectiles.projectiles is actual array inside Projectiles class
+            if (self.graphics[0].get_rect().move(self.posx, self.posy).inflate(-50, -20).colliderect(projectile.graphics[0].get_rect().move(projectile.posx, projectile.posy))) and (not projectile.isPlayers): #holy fuck
+                self.lives -= 1
+                print('player hit, ' + str(self.lives) + ' lives left')
+                projectiles.projectiles.remove(projectile)
+        
 
 class Enemy:
     def __init__(self, posx, posy, speedx, speedy):
@@ -48,35 +56,71 @@ class Enemies:
     def update(self, projectiles):
         for enemy in self.enemies:
             enemy.updatePosition()
-            if enemy.posy < -80: #TODO: sprite sizes
+            if enemy.posx < -80: #TODO: sprite sizes
                 self.enemies.remove(enemy)
-            for projectile in projectiles.projectiles:
-                if enemy.graphics[0].get_rect().move(enemy.posx, enemy.posy).inflate(-20, -20).colliderect(projectile.graphics[0].get_rect().move(projectile.posx, projectile.posy)):
+            for projectile in projectiles.projectiles: #check collisions with projectiles, projectiles.projectiles is actual array inside Projectiles class
+                if (enemy.graphics[0].get_rect().move(enemy.posx, enemy.posy).inflate(-20, -20).colliderect(projectile.graphics[0].get_rect().move(projectile.posx, projectile.posy))) and projectile.isPlayers: #holy fuck
                     print('enemy hit')
                     self.enemies.remove(enemy)
                     projectiles.projectiles.remove(projectile)
     def draw(self, screen):
         for enemy in self.enemies:
             enemy.draw(screen)
+    def checkAndSpawn(self):
+        print(len(self.enemies))
+        if len(self.enemies) <= 2:
+            self.spawnWave()
+    def spawnWave(self):
+        key = random.randint(0, 4)
+        if key == 0:
+            self.spawnWave0()
+        elif key == 1:
+            self.spawnWave1()
+        elif key == 2:
+            self.spawnWave2()
+        elif key == 3:
+            self.spawnWave3()
+        elif key == 4:
+            self.spawnWave4()
+    def spawnWave0(self): #TODO: add more variations
+        for index in range(0, 6):
+            self.addEnemy(windowWidth, index * (windowHeight - 80)/5, -2, 0)
+    def spawnWave1(self):
+        for index in range(0, 6):
+            self.addEnemy(windowWidth, index * (windowHeight - 80)/5, -2, (index-3)*(-1))
+    def spawnWave2(self):
+        for index in range(0, 6):
+            self.addEnemy(index*50 + windowWidth, index * (windowHeight - 80)/5, -2, (index-3)*(-1))
+    def spawnWave3(self):
+        for index in range(0, 6):
+            self.addEnemy(index*50 + windowWidth, (windowHeight - 80)/2, -2, 0)
+    def spawnWave4(self):
+        for index in range(0, 6):
+            self.addEnemy(index*50 + windowWidth, (windowHeight - 80)/2, -2, (index-3)*(-1))
 
 class Projectile:
-    def __init__(self, posx, posy, speedx, speedy):
+    def __init__(self, posx, posy, speedx, speedy, isPlayers):
         self.posx = posx
         self.posy = posy
         self.speedx = speedx
         self.speedy = speedy
+        self.isPlayers = isPlayers
     graphics = []
     graphics.append(pygame.transform.scale(pygame.image.load('projectile.png'), (30, 7)))
+    graphics.append(pygame.transform.flip(pygame.transform.scale(pygame.image.load('projectile.png'), (30, 7)), True, False))
     def draw(self, screen):
-        screen.blit(self.graphics[0], (self.posx, self.posy))
+        if self.isPlayers:
+            screen.blit(self.graphics[0], (self.posx, self.posy))
+        else:
+            screen.blit(self.graphics[1], (self.posx, self.posy))
     def updatePosition(self): #TODO: update this so it complies with sprite sizes
         self.posx += self.speedx
         self.posy += self.speedy
 
 class Projectiles:
     projectiles = []
-    def addProjectile(self, posx, posy, speedx, speedy):
-        self.projectiles.append(Projectile(posx, posy, speedx, speedy))
+    def addProjectile(self, posx, posy, speedx, speedy, isPlayers):
+        self.projectiles.append(Projectile(posx, posy, speedx, speedy, isPlayers))
     def update(self):
         for projectile in self.projectiles:
             projectile.updatePosition()
@@ -91,7 +135,7 @@ def loadResources():
     waveSurface = pygame.image.load('wave.png')
 
 baseWaveSpeed = 2 
-waveSpacing = 0.3
+waveSpacing = 0.3 #majic numbers
 wavePositions = []
 for i in range(64, windowHeight, 64):
     wavePositions.append([windowWidth, i])
@@ -106,13 +150,13 @@ def drawWaves(screen):
 
 
 def gameLoop():
-    player = Player(50, 280)
+    player = Player(50, 280, 3)
 
     enemies = Enemies()
-    enemies.addEnemy(560, 280, -1, 0)
+    enemies.spawnWave()
 
     projectiles = Projectiles()
-    projectiles.addProjectile(0, 280, 5, 0)
+    projectiles.addProjectile(640, 300, -5, 0, False)
 
     while 1:
         for event in pygame.event.get():
@@ -122,12 +166,15 @@ def gameLoop():
 
         screen.fill(pygame.Color('#496ddb'))
         drawWaves(screen)
+
+        player.checkProjectileCollisions(projectiles)
         player.draw(screen)
 
         projectiles.update()
         projectiles.draw(screen)
 
-        enemies.update(projectiles)
+        enemies.update(projectiles) #checking for collisions happens here
+        enemies.checkAndSpawn()
         enemies.draw(screen)
 
         pygame.display.flip()
@@ -135,7 +182,7 @@ def gameLoop():
 
 loadResources()
 
-pygame.mixer.music.load('music.ogg')
-pygame.mixer.music.play(-1)
+pygame.mixer.music.load('music.ogg') #kickass tunes
+pygame.mixer.music.play(-1) #it irritates me atm
 
 gameLoop()
